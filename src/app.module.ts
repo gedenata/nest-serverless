688ff7f42f881565d6cc04f30998863dc7b5e6f4
gedenata <igedetirtanata@gmail.com>
2021-10-09 14:45:56 +0800

    (feat): setup swagger api documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { CoreController } from './core/core.controller';
 import { CoreModule } from './core/core.module';
 
 @Module({
   imports: [AuthModule, ConfigModule.forRoot({ isGlobal: true }), CoreModule],
-  controllers: [AppController, CoreController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AppLogger } from './core/modules/logger/logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useLogger(new AppLogger());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Nest Serverless API')
+    .setDescription('API Documentation')
+    .setVersion('1.0.0')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, swaggerDocument, {
+    customCss: '#swagger-ui .topbar { display: none }',
+  });
+
   const configService = app.get(ConfigService);
   const port = configService.get('API_PORT');
 
